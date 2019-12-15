93174c0f6b5ae6e3bc96889c2f3dba3c948950e3
Justice Ampofo <justiceampofo@gmail.com>
2019-12-15 15:52:59 -0500

    Inital push and Navbar

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <nav className="nav-bar green">
+            <div className="nav-wrapper">
+                <a href="index.html" className="brand-logo center">Currency Converter</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar

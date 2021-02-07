dc1f483de990282a771b7907e66313e44a8bb79a
Justice Ampofo <justiceampofo@gmail.com>
2021-02-06 21:49:51 -0500

    including build folder

---
+++ build/precache-manifest.e1dff3555b3d7f4130418c24ded92d82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692b27e84f1eb65e0bdb080f89399812",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43b0338c0ed0efd23466",
+    "url": "/static/css/2.6a906a3b.chunk.css"
+  },
+  {
+    "revision": "13acff8ba3ed14ee1d7a",
+    "url": "/static/css/main.8465c8db.chunk.css"
+  },
+  {
+    "revision": "43b0338c0ed0efd23466",
+    "url": "/static/js/2.54f947ac.chunk.js"
+  },
+  {
+    "revision": "e3dab5fe60d6e623ca3ffbb48e40be13",
+    "url": "/static/js/2.54f947ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "13acff8ba3ed14ee1d7a",
+    "url": "/static/js/main.465096f6.chunk.js"
+  },
+  {
+    "revision": "9025bcbe04d50839dd15",
+    "url": "/static/js/runtime-main.e9c338af.js"
+  }
+]);
